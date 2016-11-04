2c8f6fc58479c9220fa478a51a5c1aaef85a7e4a
wangyujingGH <1509284978@qq.com>
2016-11-04 17:14:00 +0800

    add Hystrix command

--- src/main/java/com/sselab/springboot/author/Application.java
+++ src/main/java/com/sselab/springboot/author/Application.java
@@ -4,11 +4,13 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication(
 		scanBasePackages = "com.sselab.springboot.author"
 		)
+@EnableCircuitBreaker
 public class Application {
 	
 	private static Log log = LogFactory.getLog(Application.class);
--- src/main/java/com/sselab/springboot/author/service/impl/AuthorServiceImpl.java
+++ src/main/java/com/sselab/springboot/author/service/impl/AuthorServiceImpl.java
@@ -1,15 +1,15 @@
 package com.sselab.springboot.author.service.impl;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.sselab.springboot.author.dao.AuthorDao;
 import com.sselab.springboot.author.form.AuthorForm;
 import com.sselab.springboot.author.form.AuthorUpdateForm;
 import com.sselab.springboot.author.model.AuthorModel;
 import com.sselab.springboot.author.service.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 
@@ -20,44 +20,66 @@ public class AuthorServiceImpl implements AuthorService {
     private AuthorDao authorDao;
 
     @Override
-	public AuthorModel getAuthorInfo(String bookname){    	
-		return authorDao.getInfo(bookname);
-	}
+    @HystrixCommand(fallbackMethod = "getAuthorInfoFB")
+    public AuthorModel getAuthorInfo(String bookname){
+        return authorDao.getInfo(bookname);
+    }
 
-	@Override
-	public long add(AuthorForm form) {
-		// TODO Auto-generated method stub
-		AuthorModel model = new AuthorModel(null, form.getAuthorname(), form.getEmail(), form.getAddress());
-		authorDao.insert(model);
+    public AuthorModel getAuthorInfoFB(String bookname){
+        return null;
+    }
+
+    @Override
+    @HystrixCommand(fallbackMethod = "addFB")
+    public long add(AuthorForm form) {
+        // TODO Auto-generated method stub
+        AuthorModel model = new AuthorModel(null, form.getAuthorname(), form.getEmail(), form.getAddress());
+        authorDao.insert(model);
         return model.getAuthorId();
-	}
+    }
 
-	@Override
-	public long update(AuthorUpdateForm form) {
-		// TODO Auto-generated method stub
-		AuthorModel model = new AuthorModel(form.getAuthorId(), form.getAuthorname(), form.getEmail(), form.getAddress());
-		authorDao.update(model);
+    public long addFB(AuthorForm form) {  return 0l;    }
+
+    @Override
+    @HystrixCommand(fallbackMethod = "updateFB")
+    public long update(AuthorUpdateForm form) {
+        // TODO Auto-generated method stub
+        AuthorModel model = new AuthorModel(form.getAuthorId(), form.getAuthorname(), form.getEmail(), form.getAddress());
+        authorDao.update(model);
         return model.getAuthorId();
-	}
+    }
 
-	@Override
-	public int deleteById(long authorId) {
-		// TODO Auto-generated method stub
-		return authorDao.deleteById(authorId);
-	}
+    public long updateFB(AuthorUpdateForm form){
+        return 0l;
+    }
 
-	@Override
-	public AuthorModel getById(long authorId) {
-		// TODO Auto-generated method stub
-		return authorDao.selectById(authorId);
-	}
+    @Override
+    @HystrixCommand(fallbackMethod = "deleteByIdFB")
+    public int deleteById(long authorId) {
+        // TODO Auto-generated method stub
+        return authorDao.deleteById(authorId);
+    }
 
-	@Override
-	public List<AuthorModel> selectPaged(int page, int limits) {
-		// TODO Auto-generated method stub
-		List<AuthorModel> list = authorDao.selectPaged(page, limits);
-		return list;
-	}    
+    public int deleteByIdFB(long authorId) {  return 0;   }
+
+    @Override
+    @HystrixCommand(fallbackMethod = "getByIdFB")
+    public AuthorModel getById(long authorId) {
+        // TODO Auto-generated method stub
+        return authorDao.selectById(authorId);
+    }
+
+    public AuthorModel getByIdFB(long authorId) { return null;    }
+
+    @Override
+    @HystrixCommand(fallbackMethod = "selectPagedFB")
+    public List<AuthorModel> selectPaged(int page, int limits) {
+        // TODO Auto-generated method stub
+        List<AuthorModel> list = authorDao.selectPaged(page, limits);
+        return list;
+    }
+
+    public List<AuthorModel> selectPagedFB(int page, int limits) {    return  null;   }
 }
 
 
